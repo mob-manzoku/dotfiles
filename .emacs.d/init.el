c7ced4e3c85f3dabb17892c2c33f17bef6e639df
mob-manzoku <manzoku@mobcast.jp>
2017-01-06 21:05:48 +0900

    Update dotfiles

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,14 @@
 
 ;;; el-get
 ;;; code:
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
@@ -59,8 +67,8 @@
 (el-get-bundle web-mode)
 (el-get-bundle js2-mode)
 (el-get-bundle json-mode)
-(el-get-bundle tern)
-(el-get-bundle tern-auto-complete)
+;; (el-get-bundle tern)
+;; (el-get-bundle tern-auto-complete)
 
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 
@@ -121,3 +129,15 @@
 
 ;;; init.el ends here
 (put 'upcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (tern-auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
