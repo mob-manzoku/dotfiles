60bf3bcdbdcf86a4a8cb55148edbd37fc22b213a
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-12-18 07:32:39 +0000

    Textlint check Markdown mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -130,6 +130,19 @@
 
 ;; etc
 (el-get-bundle markdown-mode)
+(flycheck-define-checker textlint
+  "A linter for prose."
+  :command ("textlint" "--format" "unix" source-inplace)
+  :error-patterns
+  ((warning line-start (file-name) ":" line ":" column ": "
+	    (id (one-or-more (not (any " "))))
+	    (message (one-or-more not-newline)
+		     (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+	    line-end))
+  :modes (text-mode markdown-mode gfm-mode))
+(add-to-list 'flycheck-checkers 'textlint)
+(add-hook 'gfm-mode-hook 'flycheck-mode)
+(add-hook 'markdown-mode-hook 'flycheck-mode)
 
 (el-get-bundle toml-mode)
 
