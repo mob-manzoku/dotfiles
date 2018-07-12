f3ec8943f8d966eb34696bb5d4676c8d64afdd6b
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-07-12 02:43:12 +0000

    Fix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -84,7 +84,7 @@
 (defun my-js2-mode-hook ()
   "Hooks for js2 mode."
   (setq indent-tabs-mode nil)
-  (setq js2-basic-offset 2))
+  (setq js2-basic-offset 4))
 
 (add-hook 'js2-mode-hook  'my-js2-mode-hook)
 
